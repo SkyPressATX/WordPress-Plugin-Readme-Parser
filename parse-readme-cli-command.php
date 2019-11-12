5c78ef53b3bef5423d18f6fc3ac45dfa840f141e
Mark Kelnar <markkelnar@gmail.com>
2019-11-11 21:07:11 -0600

    Add wp cli command

---
+++ parse-readme-cli-command.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Parse readme command
+ *
+ * <message>
+ * : An awesome message to display
+ *
+ * --append=<message>
+ * : An awesome message to append to the original message.
+ *
+ * @when before_wp_load
+ */
+
+namespace ReadmeParser;
+
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+    require_once( 'parse-readme.php' );
+
+    function parse( $args, $assoc_args ) {
+        $file = $assoc_args['file'];
+        $outfile = "$file.dat";
+        if ( ! $file ) {
+            \WP_CLI::error('You need to specify a readme file. Ex. --file=readme.txt');
+        }
+        if ( ! is_readable($file) ) {
+            \WP_CLI::error("File '$file' is not readable");
+        }
+        $p = new \WordPress_Readme_Parser();
+        $o = $p->parse_readme( $assoc_args['file'] );
+        $data = serialize($o);
+        if ( false === file_put_contents($outfile, $data) ) {
+            \WP_CLI::error("There was an error saving parsed data to file $outfile");
+        }
+        \WP_CLI::success( $outfile );
+    };
+
+    \WP_CLI::add_command( 'parse', __NAMESPACE__ . '\parse' );
+}
